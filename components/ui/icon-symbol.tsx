62de30947b8112ca30e2cf053d696b014c32205f
alaninnovates <alanlichen1@gmail.com>
2026-08-22 08:25:41 -0700

    Add icon mappings for navbar icons

--- components/ui/icon-symbol.tsx
+++ components/ui/icon-symbol.tsx
@@ -18,6 +18,9 @@ const MAPPING = {
   'paperplane.fill': 'send',
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
+  'theatermasks.fill': 'theater-comedy',
+  'person.3.fill': 'groups',
+  'person.fill': 'person',
 } as IconMapping;
 
 /**
