c48801881377c6736f02fc1d203a78865fbf17ea
alaninnovates <alanlichen1@gmail.com>
2026-08-30 23:49:26 -0700

    Fix step size calculation

--- components/field/parser.ts
+++ components/field/parser.ts
@@ -139,12 +139,14 @@ export const calculateStepSize = (
     coord2: Coordinate,
     counts: number,
 ): number => {
-    // console.log('coord1:', coord1, 'coord2:', coord2);
+    console.log('coord1:', coord1, 'coord2:', coord2);
     const stepsDelta = Math.sqrt(
         Math.pow(coord2.x - coord1.x, 2) +
         Math.pow(coord2.y - coord1.y, 2),
     );
+    // console.log('stepsDelta:', stepsDelta);
     const yards = stepsToYards(stepsDelta);
     // console.log('yards between dots:', yards);
-    return Math.round((8 * yards / counts) * 100) / 100;
+    // console.log('counts:', counts);
+    return Math.round(counts * 5 / yards * 100) / 100;
 };
